1c090fe660ab4e540b38a99a787ae848bb4ecbc7
onchezz <brianonchezz@gmail.com>
2023-07-19 18:52:50 +0300

    init

---
+++ contracts/Subscription.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.10;
+
+contract Subscription {
+    struct Subscriber {
+        bool subscribed;
+        uint256 payedAmount;
+        uint256 subscribedAt;
+    }
+
+    event SubscriptionSuccess(address indexed from, uint256 subscribedAt);
+
+    address public owner;
+    uint256 public subscriptionBaseValue;
+    uint256 public subscriptionDuration;
+    address[] public subscribers;
+    mapping(address => Subscriber) private subscribersList;
+
+    constructor(uint256 _baseValue, uint256 _durationInMinutes) {
+        subscriptionBaseValue = _baseValue;
+        subscriptionDuration = _durationInMinutes * 1 minutes;
+        owner = msg.sender;
+        subscribersList[owner] = Subscriber(true, 0, block.timestamp);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not Owner");
+        _;
+    }
+
+    modifier exactAmount() {
+        require(msg.value == subscriptionBaseValue, "Insufficient funds");
+        _;
+    }
+
+    function isSubscriptionValid(address subscriberAddress) private view returns (bool) {
+        if( subscriberAddress == owner ) return true;
+        bool isSubscribed = subscribersList[subscriberAddress].subscribed == true;
+        uint256 limitDate = subscribersList[subscriberAddress].subscribedAt + subscriptionDuration;
+        return isSubscribed && block.timestamp <= limitDate;
+    }
+
+    function subscribe() external payable exactAmount {
+        address target = msg.sender;
+        if (subscribersList[target].subscribed == true) {
+            require(!isSubscriptionValid(target), "Subscription is still active");
+            subscribersList[target].payedAmount += msg.value;
+            subscribersList[target].subscribedAt = block.timestamp;
+        } else {
+            Subscriber memory newSubscriber = Subscriber(true, msg.value, block.timestamp);
+            subscribersList[target] = newSubscriber;
+            subscribers.push(target);
+        }
+        emit SubscriptionSuccess(target, block.timestamp);
+    }
+
+    function getSubscriberData(address subscriberAddress) private view returns (Subscriber memory data) {
+        bool isSubscribed = isSubscriptionValid(subscriberAddress);
+        uint256 at = subscribersList[subscriberAddress].subscribedAt;
+        uint256 payedAmount = subscribersList[subscriberAddress].payedAmount;
+        data = Subscriber(isSubscribed, payedAmount, at);
+        return data;
+    }
+
+    function getDataOfSubscriber(address subscriberAddress) external view onlyOwner returns (Subscriber memory data) {
+        return getSubscriberData(subscriberAddress);
+    }
+
+    function getSubscriptionData() external view returns (Subscriber memory data) {
+        return getSubscriberData(msg.sender);
+    }
+
+    function getAllSubscribers() public view onlyOwner returns (address[] memory) {
+        return subscribers;
+    }
+    
+    function getBalance() external view onlyOwner returns (uint256) {
+        return address(this).balance;
+    }
+
+    function remove() external onlyOwner {
+        selfdestruct(payable(owner));
+    }
+
+    function withdraw() external onlyOwner {
+        (bool sent, ) = msg.sender.call{value: address(this).balance}("");
+        require(sent, "Failed to send Ether");
+    }
+
+}
---
+++ test/Subscription.test.js
@@ -0,0 +1,180 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
+const Subscription = artifacts.require('./Subscription.sol');
+const timeMachine = require('ganache-time-traveler');
+
+const { subscriptionValue, durationInMinutes } = require('../environment.json');
+
+contract('Subscription Contract should', (accounts) => {
+  let contractUnderTest;
+  let owner;
+  let subscriber;
+  let snapshotId;
+
+  beforeEach(async () => {
+    const snapshot = await timeMachine.takeSnapshot();
+    snapshotId = snapshot.result;
+    [owner, subscriber] = accounts;
+    contractUnderTest = await Subscription.new(subscriptionValue, durationInMinutes, { from: owner });
+  });
+
+  afterEach(async () => {
+    await timeMachine.revertToSnapshot(snapshotId);
+  });
+
+  it("return owner's address", async () => {
+    const ownerAddress = await contractUnderTest.owner.call({ from: owner });
+    expect(ownerAddress).to.equal(owner);
+  });
+
+  it('return balance 0 by default', async () => {
+    const balance = await contractUnderTest.getBalance.call({ from: owner });
+    expect(balance.toNumber()).to.equal(0);
+  });
+
+  it('return an error if other but the owner asks for balance', async () => {
+    const ERROR_MSG = 'Returned error: VM Exception while processing transaction: revert Not Owner';
+    try {
+      await contractUnderTest.getBalance.call({ from: subscriber });
+    } catch (error) {
+      expect(error.message).to.equal(ERROR_MSG);
+    }
+  });
+
+  it('register a new subscriber and update the balance if the payed amount is correct', async () => {
+    await contractUnderTest.subscribe({
+      from: subscriber,
+      value: subscriptionValue,
+    });
+    const balance = await contractUnderTest.getBalance.call({ from: owner });
+    expect(balance.toString()).to.equal(subscriptionValue);
+  });
+
+  it('emit an event when the subscription succeeds', async () => {
+    const transaction = await contractUnderTest.subscribe({
+      from: subscriber,
+      value: subscriptionValue,
+    });
+
+    const events = transaction.logs;
+    expect(events.length).to.equal(1);
+    const event = events[0];
+    expect(event.args.from).to.equal(subscriber);
+  });
+
+  it('return an error if the ammount sent to perform the subscription is invalid', async () => {
+    const ERROR_REASON = 'Insufficient funds';
+    try {
+      await contractUnderTest.subscribe({
+        from: subscriber,
+        value: subscriptionValue - 1,
+      });
+    } catch (error) {
+      expect(error.reason).to.equal(ERROR_REASON);
+    }
+  });
+
+  it('return an error if the subscriber attempts to resuscribe before expiring current subscription', async () => {
+    const ERROR_REASON = 'Subscription is still active';
+    try {
+      await contractUnderTest.subscribe({
+        from: subscriber,
+        value: subscriptionValue,
+      });
+      await contractUnderTest.subscribe({
+        from: subscriber,
+        value: subscriptionValue,
+      });
+    } catch (error) {
+      expect(error.reason).to.equal(ERROR_REASON);
+      const balance = await contractUnderTest.getBalance.call({ from: owner });
+      expect(balance.toString()).to.equal(subscriptionValue);
+    }
+  });
+
+  it('return proper data if an unsubscribed user attempts to retrieve its status', async () => {
+    const subscriptionData = await contractUnderTest.getSubscriptionData.call({ from: subscriber });
+    expect(subscriptionData.subscribed).to.be.false;
+    expect(subscriptionData.payedAmount).to.equal('0');
+    expect(subscriptionData.subscribedAt).to.equal('0');
+  });
+
+  it('return "false" if the subcription has expired', async () => {
+    await contractUnderTest.subscribe({
+      from: subscriber,
+      value: subscriptionValue,
+    });
+    await timeMachine.advanceTimeAndBlock(durationInMinutes * 60 + 1);
+    const subscriptionData = await contractUnderTest.getSubscriptionData.call({ from: subscriber });
+    expect(subscriptionData.subscribed).to.be.false;
+  });
+
+  it('allow a subscriber to renew its subscription after it has expired', async () => {
+    await contractUnderTest.subscribe({
+      from: subscriber,
+      value: subscriptionValue,
+    });
+    await timeMachine.advanceTimeAndBlock(durationInMinutes * 60 + 1);
+    await contractUnderTest.subscribe({
+      from: subscriber,
+      value: subscriptionValue,
+    });
+    const subscriptionData = await contractUnderTest.getSubscriptionData.call({ from: subscriber });
+    expect(subscriptionData.subscribed).to.be.true;
+  });
+
+  it('retrieve data of a subscriber if the request comes from the owner', async () => {
+    await contractUnderTest.subscribe({
+      from: subscriber,
+      value: subscriptionValue,
+    });
+    const subscriberData = await contractUnderTest.getDataOfSubscriber(subscriber, {
+      from: owner,
+    });
+    expect(subscriberData.subscribed).to.be.true;
+    expect(subscriberData.payedAmount).to.equal(subscriptionValue.toString());
+    expect(subscriberData.subscribedAt).to.not.equal(0);
+  });
+
+  it('not retrieve data of a subscriber if the request comes from anyone else but the owner', async () => {
+    const ERROR_MSG = 'Returned error: VM Exception while processing transaction: revert Not Owner';
+    try {
+      await contractUnderTest.subscribe({
+        from: subscriber,
+        value: subscriptionValue,
+      });
+      await contractUnderTest.getDataOfSubscriber(subscriber, {
+        from: subscriber,
+      });
+    } catch (error) {
+      expect(error.message).to.equal(ERROR_MSG);
+    }
+  });
+
+  it('retrieve the list of subscribers to the owner', async () => {
+    await contractUnderTest.subscribe({
+      from: subscriber,
+      value: subscriptionValue,
+    });
+    const subscribers = await contractUnderTest.getAllSubscribers.call({
+      from: owner,
+    });
+    expect(subscribers.length).to.equal(1);
+    expect(subscribers[0]).to.equal(subscriber);
+  });
+
+  it('not retrieve the list of subscribers if the request comes from anyone else but the owner', async () => {
+    const ERROR_MSG = 'Returned error: VM Exception while processing transaction: revert Not Owner';
+    try {
+      await contractUnderTest.subscribe({
+        from: subscriber,
+        value: subscriptionValue,
+      });
+      await contractUnderTest.getAllSubscribers.call({
+        from: owner,
+      });
+    } catch (error) {
+      expect(error.message).to.equal(ERROR_MSG);
+    }
+  });
+});
